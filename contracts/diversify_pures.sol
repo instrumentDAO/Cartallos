ef541bc0d6f9a93455a34548ff7152fc77902054
Tw33t3r <31904868+Tw33t3r@users.noreply.github.com>
2021-04-05 09:49:39 -0600

    Structure for pancakeswap token swapping

--- contracts/diversify_pures.sol
+++ contracts/diversify_pures.sol
@@ -4,12 +4,17 @@ pragma solidity =0.7.5;
 
 import "./BEP20/BEP20.sol";
 import "./BEP20/IBEP20.sol";
+import "./Interfaces/IPancakeRouter02.sol";
+
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract CartallosPures is BEP20{
     using SafeMath for uint256;
 
-//BTCB
+    address internal constant PANCAKESWAP_ROUTER_ADDRESS = 0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F;
+    IPancakeRouter02 public pancakeswapRouter;
+
+
     address a_btc = 0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c;
     address a_eth = 0x2170Ed0880ac9A755fd29B2688956BD959F933F8;
     address a_wbnb = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
@@ -26,6 +31,7 @@ contract CartallosPures is BEP20{
 
 
     constructor() BEP20("Cartallos General Pool", "DRV-G") {
+        pancakeswapRouter = IPancakeRouter02(PANCAKESWAP_ROUTER_ADDRESS);
         assets[address(btc)] = true;
         assets[address(eth)] = true;
         assets[address(wbnb)] = true;
@@ -36,12 +42,26 @@ contract CartallosPures is BEP20{
         assetPerCartallosToken[wbnb] = 10 * gweiUnits / 10; //.1 tokens to 1
 
     }
-
+/*
+TODO
+DECIDE ON DEADLINE instead of block.timestamp SHOULD PASS FROM FRONTEND
+*/
     function mint(uint256 amount) public{
-        bool success = btc.transferFrom(msg.sender, address(this), (assetPerCartallosToken[btc] * amount) / gweiUnits);
-        success = success && eth.transferFrom(msg.sender, address(this), (assetPerCartallosToken[eth] * amount) / gweiUnits);
-        success = success && wbnb.transferFrom(msg.sender, address(this), (assetPerCartallosToken[wbnb] * amount) / gweiUnits);
-        require(success, "could not transfer all assets to the contract, check balances and approvals");
+        address[] memory path = new address[](2);
+        path[0] = pancakeswapRouter.WETH();
+        path[1] = address(a_btc);
+        //docs say 5 inputs (amountIn, amountOutMin, path, to, deadline)
+        //Compiler demands 4 inputs though, I think amountIn isn't considered necessary??..
+/*
+TODO
+Handle data from pancakeswap router, insert requires where necessary
+*/
+        pancakeswapRouter.swapExactETHForTokens(((assetPerCartallosToken[btc] * amount) / gweiUnits), path, address(this), block.timestamp);        
+        //is reusing path like this okay??
+        path[1] = address(a_eth);
+        pancakeswapRouter.swapExactETHForTokens(((assetPerCartallosToken[eth] * amount) / gweiUnits), path, address(this), block.timestamp); 
+        //should we even bother wrapping the bnb??
+        //require(a_wbnb.deposit((assetPerCartallosToken[eth] * amount) / gweiUnits));
         _mint(msg.sender, amount);
     }
 
