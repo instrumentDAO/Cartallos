7a12172d7bc84ee47164a77e9e63af8e5deb6e0b
Tw33t3r <31904868+Tw33t3r@users.noreply.github.com>
2021-04-07 19:14:52 -0600

    added real BEP20 directory

---
+++ contracts/BEP20/BEP20.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.7.5;
+
+import "./OpenZeppelinContracts/ERC20.sol";
+import "./OpenZeppelinContracts/Ownable.sol";
+
+contract BEP20 is ERC20, Ownable {
+    constructor (string memory name_, string memory symbol_) ERC20(name_, symbol_){
+
+    }
+    function getOwner() external view returns (address){
+        return owner();
+    }
+}
