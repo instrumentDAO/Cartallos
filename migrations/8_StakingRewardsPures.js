e452f65f442ec2325bb250ab4dc8809295475ca0
dak <mind ur own bussiness>
2021-07-17 17:47:38 -0600

    added functionality to fill pools to also create and fill cartallos governance token pool

--- migrations/8_StakingRewardsPures.js
+++ migrations/8_StakingRewardsPures.js
@@ -5,15 +5,15 @@ const contract = JSON.parse(fs.readFileSync('../contracts/StakingRewards.json'))
 const abi = contract['abi'];
 const bytecode = contract['bytecode'];
 //governance token
-const rewardsToken = '0x7907d0C11B358dd1229C9332D85fA22783658bD4';
+const cartallosGovernanceToken = '0x7907d0C11B358dd1229C9332D85fA22783658bD4';
 //cart-pures
-const stakingToken = '0x51a255540d8c915eC9f846A00044B864Bbd672D9';
+const cartallosGeneralToken = '0x51a255540d8c915eC9f846A00044B864Bbd672D9';
 
 module.exports = function(deployer, network, accounts) {
     let web3 = new Web3();
     web3.setProvider(provider);
     let stakingRewards = new web3.eth.Contract(abi);
-    stakingRewards.deploy({data: bytecode, arguments: [accounts[0], accounts[0], rewardsToken, stakingToken]}).send({from: accounts[0], gas: 6721975, gasPrice: '20000000000'}).then(
+    stakingRewards.deploy({data: bytecode, arguments: [accounts[0], accounts[0], cartallosGovernanceToken, cartallosGeneralToken]}).send({from: accounts[0], gas: 6721975, gasPrice: '20000000000'}).then(
         function(result){
             console.log(result.options.address);
         }
--- test/FillPools.js
+++ test/FillPools.js
@@ -7,6 +7,7 @@ const provider = new Web3.providers.HttpProvider('http://localhost:8545');
 const wBNBAddress = '0x6DFAFB92fafA78E82802fFA07CCCE1dcD05Ec9de';
 const eTHAddress = '0x54824f67455A05Cad7b0751c4715e46C2aa226C2';
 const bTCAddress = '0xe778f496CE179f3895Fb10E040E4dA85A31e2724';
+const cartGovAddress = '0x7907d0C11B358dd1229C9332D85fA22783658bD4';
 const uniswapAddress = '0x68eA183eDbfc146407af1672D2d1cc351c2fb5b8';
 const owner = '0x6a2B6283AD99b412b717564c068Ab8Bd97294AC4';
 const account = '0x3e6E59c45c65F4Da3be7fB8bf67eef54B84B287b';
@@ -19,6 +20,9 @@ const eTHAbi = eTHJson['abi'];
 
 const bTCJson = JSON.parse(fs.readFileSync('../build/contracts/BTC.json'));
 const bTCAbi = bTCJson['abi'];
+
+const cartGovJson = JSON.parse(fs.readFileSync('../build/contracts/Cartallos.json'));
+const cartGovAbi = cartGovJson['abi'];
 
 const uniswapJson = require('../contracts/TestTokens/UniswapV2Router02.json');
 const uniswapAbi = uniswapJson['abi'];
@@ -36,7 +40,10 @@ module.exports = function(){
         wBNB = await new web3.eth.Contract(wBNBAbi, wBNBAddress);
         eTH = await new web3.eth.Contract(eTHAbi, eTHAddress);
         bTC = await new web3.eth.Contract(bTCAbi, bTCAddress);
+        cartGov = await new web3.eth.Contract(cartGovAbi, cartGovAddress);
         uniswap = await new web3.eth.Contract(uniswapAbi, uniswapAddress);
+
+        // fill the pools for eth/bnb and btc/bnb
         wBNB.methods.deposit().send({from: owner, value: 20000000000000000000}).then(function(wBNBDeposit){
             eTH.methods.mint((wei2eth.mul(web3.utils.toBN('100')))).send({from: owner}).then(function(eTHMint){
                 bTC.methods.mint((wei2eth.mul(web3.utils.toBN('100')))).send({from: owner}).then(function(bTCMint){
@@ -50,6 +57,16 @@ module.exports = function(){
                     })
                 })
                         })
+
+
+            //fill the cartallos governance pool
+            wBNB.methods.deposit().send({from: account, value: 20000000000000000000}).then(function(wBNBDeposit){
+                    wBNB.methods.approve(uniswapAddress, (wei2eth.mul(web3.utils.toBN('20')))).send({from: account}).then(function(approved){
+                        cartGov.methods.approve(uniswapAddress, (wei2eth.mul(web3.utils.toBN('10')))).send({from: account}).then(function(cgapp){
+                                uniswap.methods.addLiquidity(cartGovAddress, wBNBAddress, (wei2eth), (wei2eth), (2000000000000000), (2000000000000000), account, 1651311457).send({from: account, gas: 6721975, gasPrice: '20000000000'});
+                            })
+                        })
+                })
         }).catch(function(err){
                 console.log("error: " + err.message);
             });
