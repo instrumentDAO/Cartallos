887ee1978d15cc112494d01047fac6660e1d1791
dak <mind ur own bussiness>
2021-08-01 01:47:14 -0600

    added setupPresale script to the setupContracts script, transfers tokens to the presale contract so it is functional completely after the setupContracts

--- setupContracts.sh
+++ setupContracts.sh
@@ -9,6 +9,8 @@ echo "Filling index token pools"
 truffle exec ./test/FillCartGPool.js
 echo "Setting up staking pools"
 truffle exec test/SetupStakingPools.js
+echo "Filling Presale Contract"
+truffle exec test/SetupPresale.js
 echo "creating snapshot"
 curl -H "Content-Type: application/json" -X POST --data \
         '{"id":1337,"jsonrpc":"2.0","method":"evm_snapshot","params":[]}' \
