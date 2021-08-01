805ee6080cd0063d102292da32fc0296139f1515
dak <mind ur own bussiness>
2021-08-01 15:59:03 -0600

    added start and stop presale functionality, depending on duration and block timestamp. All unsold tokens are transfered back to the presale owner at the end of the presale, along with all the eth/bnb/matic

--- contracts/Presale.sol
+++ contracts/Presale.sol
@@ -12,36 +12,51 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 
 
 contract Presale is Ownable {
-using SafeMath for uint256;
-IERC20 saleToken;
-uint256 saleRatioTimesEthUnits; //sale price is
-uint256 ethUnits = 1000000000000000000;
+    using SafeMath for uint256;
+    IERC20 saleToken;
+    uint256 saleRatioTimesEthUnits; //sale price is
+    uint256 ethUnits = 1000000000000000000;
+    uint256 endDate = 0;
 
 
-constructor (address sale_Token, uint256 sale_Price) {
-saleToken = IERC20(sale_Token);
-saleRatioTimesEthUnits = sale_Price;
-}
+    constructor (address sale_Token, uint256 sale_Price) {
+        saleToken = IERC20(sale_Token);
+        saleRatioTimesEthUnits = sale_Price;
+    }
 
 
-//amount is in wei
-//saleRatioTimesEthUnits is the saleprice multiplied by ethUnits, so it doesnt have to be fraction
-//if 1 of the sale token costs 5 matic, 5000000000000000000, aka 5e18
-//if 1 of the sale token costs .5 matic, 500000000000000000 aka 5e17
+    //amount is in wei
+    //saleRatioTimesEthUnits is the saleprice multiplied by ethUnits, so it doesnt have to be fraction
+    //if 1 of the sale token costs 5 matic, 5000000000000000000, aka 5e18
+    //if 1 of the sale token costs .5 matic, 500000000000000000 aka 5e17
 
-// ethRequiredToSpend = (tokensWanted * saleRatioTimesEthUnits) / saleRatioTimesEthUnits
-// saleRatioTimesEthUnits/saleRatioTimesEthUnits is really the faction that represents the price, but we use communtive math to make solidity not have to do decimals
+    // ethRequiredToSpend = (tokensWanted * saleRatioTimesEthUnits) / saleRatioTimesEthUnits
+    // saleRatioTimesEthUnits/saleRatioTimesEthUnits is really the faction that represents the price, but we use communtive math to make solidity not have to do decimals
 
-//lets us basically use fraction without using fraction. Only has 1e18 units of precision tho
-
-function buyPresaleToken(uint256 amount) public payable{
-require(saleToken.balanceOf(address(this)) >= amount, "Not Enough Presale Tokens Remain");
-require(msg.value ==  (amount * saleRatioTimesEthUnits) / ethUnits, "Incorrect Message Value Sent" );
-saleToken.transfer(msg.sender, amount);
-}
+    //lets us basically use fraction without using fraction. Only has 1e18 units of precision tho
 
 
-function collectFunds() public onlyOwner{
-payable(owner()).transfer(address(this).balance);
-}
+    function startPresale(uint256 duration) public onlyOwner{
+        endDate = block.timestamp + duration;
+    }
+
+
+    function endPresale() public onlyOwner{
+        require(block.timestamp > endDate);
+        collectFunds();
+        saleToken.transfer(owner(), saleToken.balanceOf(address(this)));
+    }
+
+
+    function buyPresaleToken(uint256 amount) public payable{
+        require(saleToken.balanceOf(address(this)) >= amount, "Not Enough Presale Tokens Remain");
+        require(msg.value ==  (amount * saleRatioTimesEthUnits) / ethUnits, "Incorrect Message Value Sent" );
+        require(block.timestamp <= endDate, "presale is either over or has not started");
+        saleToken.transfer(msg.sender, amount);
+    }
+
+
+    function collectFunds() public onlyOwner{
+        payable(owner()).transfer(address(this).balance);
+    }
 }
